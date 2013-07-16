a02645e07641cdf504f903106a779dd6b5f4895a
Evan K. Friis <ekfriis@gmail.com>
2013-07-16 14:42:41 +0200

    Use the correct widths when testing on the native system.

--- src/xil_types.h
+++ src/xil_types.h
@@ -82,6 +82,7 @@
  * New simple types.
  * @{
  */
+#ifndef USE_INTTYPES
 #ifndef __KERNEL__
 #ifndef XBASIC_TYPES_H
 /**
@@ -102,6 +103,18 @@ typedef long s32;
 typedef long long s64;
 #else
 #include <linux/types.h>
+#endif
+#else
+#include <stdint.h>
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
 #endif
 
 
