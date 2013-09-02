e444667f2e15e29ab7a3cede8bf6a220a49b5e9b
Austin Belknap <dabelknap@wisc.edu>
2013-09-02 15:12:37 +0200

    Create named pipes in /tmp directory

--- tests/vme2fd_tests.sh
+++ tests/vme2fd_tests.sh
@@ -2,28 +2,28 @@
 
 TEST_STRING="Put that in your pipe and smoke it\nNewline"
 
-mkfifo in_stream
-mkfifo out_stream
+mkfifo /tmp/$USER/in_stream
+mkfifo /tmp/$USER/out_stream
 
-./bin/vme2fd in_stream out_stream &
+./bin/vme2fd /tmp/$USER/in_stream /tmp/$USER/out_stream &
 
 echo -e $TEST_STRING > test.in
-cat test.in > in_stream
+cat test.in > /tmp/$USER/in_stream
 
-cat < out_stream > test.out
+cat < /tmp/$USER/out_stream > test.out
 
 DIFF=`diff test.in test.out`
 
 if [ "$DIFF" == "" ]
 then
-    rm in_stream out_stream test.in test.out
+    rm /tmp/$USER/in_stream /tmp/$USER/out_stream test.in test.out
     exit 0
 else
     echo "vme2fd_test"
     echo "----------"
     cat test.in
     cat test.out
-    rm in_stream out_stream test.in test.out
+    rm /tmp/$USER/in_stream /tmp/$USER/out_stream test.in test.out
     echo ""
     exit 1
 fi
