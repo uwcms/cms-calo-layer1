f5705e9f2026fca857eccd0a59320c3d123e035d
Austin Belknap <dabelknap@wisc.edu>
2013-09-23 15:16:29 +0200

    Temporary compiling scrips (To Do: fix Makefile)

---
+++ compile_tmp.sh
@@ -0,0 +1,6 @@
+export SOFTDIR=/afs/cern.ch/user/d/dbelknap/softipbus
+export FLAGS='-g -Wall -Iinclude -I'$SOFTDIR'/include -I/opt/xdaq/include/ -DLINUX'
+export LDFLAGS='-L/opt/xdaq/lib/ -lCAENVME -llog4cplus'
+
+echo 'g++ -o bin/vme2fd '$FLAGS' '$LDFLAGS' src/vme2fd.cc src/vmestream/caen.cc src/vmestream/VMEController.cc src/vmestream/VMEStream_PC.c'$SOFTDIR'/src/circular_buffer.c '$SOFTDIR'/src/buffer.c'
+g++ -o bin/vme2fd $FLAGS $LDFLAGS src/vme2fd.cc src/vmestream/caen.cc src/vmestream/VMEController.cc src/vmestream/VMEStream_PC.c $SOFTDIR/src/circular_buffer.c $SOFTDIR/src/buffer.c src/vmestream/OrscEmulator.cc
