b834b1a6aaeedb6ebc81feedc16114878f090405
Evan Friis <ekfriis@gmail.com>
2013-10-03 08:36:32 -0500

    Use VME addresses in the actual machine
    As from Mathias, mentioned in #8

--- VMEStream/include/VMEStreamAddress.h
+++ VMEStream/include/VMEStreamAddress.h
@@ -2,10 +2,10 @@
  * Central definition of VME addresses used by VMEStream
  */
 
-#define ORSC_2_PC_SIZE 0xDEADBEEF
-#define PC_2_ORSC_SIZE 0xBEEFCAFE
+#define ORSC_2_PC_SIZE 0x0800
+#define PC_2_ORSC_SIZE 0x0000
 
-#define ORSC_2_PC_DATA 0xFACEFEED
-#define PC_2_ORSC_DATA 0xCAFEBABE
+#define ORSC_2_PC_DATA 0x0804
+#define PC_2_ORSC_DATA 0x0004
 
-#define VMERAMSIZE 512
+#define VMERAMSIZE 511 // (0x800 - 0x004)/4
