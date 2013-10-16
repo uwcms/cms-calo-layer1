8e7201fe46a533e90533e6d00064f192e8164575
Nate Woods <nwoods@hep.wisc.edu>
2013-10-16 13:04:35 -0500

    Fixed makefiles to always find code and other makefiles correctly

--- environment.sh
+++ environment.sh
@@ -3,15 +3,9 @@
 . /opt/Xilinx/14.4/ISE_DS/settings64.sh
 
 # For reference - to setup Petalinux gcc
-# pushd /afs/hep.wisc.edu/home/uwhepfpga/petalinux-v12.12-final-full
-# source settings.sh
-# popd
+pushd /afs/hep.wisc.edu/home/uwhepfpga/petalinux-v12.12-final-full
+source settings.sh
+popd
 
 # Setup an env var pointing to your softipbus installation
-export SOFTIPBUS=$HOME/trigger_code/softipbus
-
-# Include xdaq libraries
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/xdaq/lib
-
-# Run VMEStream in ECHOTEST mode (for now)
-export VME_CONTROLLER=TESTECHO
+export SOFTIPBUS=/afs/hep.wisc.edu/cms/nwoods/trigger_code/softipbus
